69b713b4df24cf62e413954eb17219ce649c9317
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-08 07:45:54 +0800

    007 Working on the Main Layout & Styling

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import router from './routes';
+import App from './App.vue';
 
-const app = createApp({});
+const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
