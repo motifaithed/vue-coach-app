85c797bdac4f08617a2dae570910c6f466ff5333
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-08 08:52:04 +0800

    010 Working on the Coaches List and List Items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,33 +1,34 @@
 <template>
-    <section>
-        FILTER
-    </section>
-    <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as a Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <li v-for="coach in listOfCoaches" :key="coach.id">{{ coach.firstName }}</li>
-        </ul>
-        <h3 v-else>There are no coach listed!</h3>
-    </section>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <coach-item v-for="coach in listOfCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName" :lastName="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+    </ul>
+    <h3 v-else>There are no coach listed!</h3>
+  </section>
 </template>
 <script>
-
-    export default {
-        computed:{
-            listOfCoaches(){
-                return this.$store.getters['coaches/coaches'];
-            },
-            hasCoaches(){
-                return this.$store.getters['coaches/hasCoaches']
-            }
-        }
-    }
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: {
+    CoachItem,
+  },
+  computed: {
+    listOfCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 <style scoped>
-    ul {
+ul {
   list-style: none;
   margin: 0;
   padding: 0;
