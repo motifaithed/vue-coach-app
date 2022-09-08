9a761ccecc702b9bc0bfd45387dd27afc8201884
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-08 08:30:29 +0800

    009 Adding Vuex and Coach Data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import router from './routes';
 import App from './App.vue';
+import store from './store/index';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,8 +7,34 @@
             <button>Refresh</button>
             <router-link to="/register">Register as a Coach</router-link>
         </div>
-        <ul>
-            LIST OF COACHES
+        <ul v-if="hasCoaches">
+            <li v-for="coach in listOfCoaches" :key="coach.id">{{ coach.firstName }}</li>
         </ul>
+        <h3 v-else>There are no coach listed!</h3>
     </section>
 </template>
+<script>
+
+    export default {
+        computed:{
+            listOfCoaches(){
+                return this.$store.getters['coaches/coaches'];
+            },
+            hasCoaches(){
+                return this.$store.getters['coaches/hasCoaches']
+            }
+        }
+    }
+</script>
+<style scoped>
+    ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
